6b525d2d003895ed35501b7221035c050290c7ec
Piyush Garg <pgarg2598.ca18@chitkara.edu.in>
2020-03-11 21:11:06 +0530

    Toast Added

--- app/src/main/java/dev/piyushgarg/authentication/RegisterActivity.java
+++ app/src/main/java/dev/piyushgarg/authentication/RegisterActivity.java
@@ -45,7 +45,7 @@ public class RegisterActivity extends AppCompatActivity {
                                         @Override
                                         public void onComplete(@NonNull Task<Void> task) {
                                             if(task.isSuccessful()){
-
+                                                Toast.makeText(getApplicationContext(),"Registration Complete",Toast.LENGTH_SHORT).show();
                                             }else{
                                                 Toast.makeText(getApplicationContext(),task.getException().toString(),Toast.LENGTH_SHORT).show();
                                             }
